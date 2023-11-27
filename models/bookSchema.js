8d08461052af8766dd87c1ca00c6245927363091
sandeep kumar <sss72778895@gmail.com>
2023-11-27 15:31:09 +0530

    commit
    Creating Book Schema

---
+++ models/bookSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required field"],
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required field!"],
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: [true, "Author is required field!"],
+    trim: true,
+  },
+  edition: {
+    type: Number,
+    required: [true, "Edition is required field!"],
+    trim: true,
+  },
+  publication: {
+    type: String,
+    required: [true, "Publication is required field!"],
+    trim: true,
+  },
+  publishedYear: {
+    type: Number,
+    required: [true, "published year is required field!"],
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
